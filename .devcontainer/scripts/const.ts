fb8ec3165a0f83cdc22082cbcb692ae19eb22919
Alfurquan Zahedi <alzahedi@microsoft.com>
2025-10-29 06:42:05 +0000

    Added devcontainer build

---
+++ .devcontainer/scripts/const.ts
@@ -0,0 +1,5 @@
+
+export const devcontainerFile = '.devcontainer/devcontainer.json'
+export const devcontainerHashFile = '.devcontainer/.devcontainer-hash.txt'
+export const name = 'devcontainer/test-runner'
+export const registry = 'testrunner.azurecr.io'
