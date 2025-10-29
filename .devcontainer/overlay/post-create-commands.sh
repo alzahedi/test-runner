46d9fc5bb928e3845fb549995030ac8d1d7d2769
Alfurquan Zahedi <alzahedi@microsoft.com>
2025-10-29 06:04:13 +0000

    Installed nx

--- .devcontainer/overlay/post-create-commands.sh
+++ .devcontainer/overlay/post-create-commands.sh
@@ -3,7 +3,12 @@
 # Install global python packages
 pip3 install --upgrade -r "/workspaces/test-runner/.devcontainer/overlay/requirements.txt"
 
-# Add Azure CLI extensiosn
+npm install
+
+# Invoke nx directly
+npm i -g nx
+
+# Add Azure CLI extensions
 az extension add --name azure-devops
 
 echo "Post-Create Commands Complete!"
